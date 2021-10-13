80002a16eda682e6bd2cdc0beb5750a1df52b18e
pilichm <michal.pilichowski@onet.pl>
2021-10-13 18:11:22 +0200

    Main activity layout creation: - ask user if he wants to save location after long press on map.

--- app/src/main/java/pl/pilichm/savemaplocation/activities/LocationMapActivity.kt
+++ app/src/main/java/pl/pilichm/savemaplocation/activities/LocationMapActivity.kt
@@ -1,6 +1,9 @@
 package pl.pilichm.savemaplocation.activities
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,7 +14,7 @@ import com.google.android.gms.maps.model.MarkerOptions
 import pl.pilichm.savemaplocation.R
 import pl.pilichm.savemaplocation.databinding.ActivityLocationMapBinding
 
-class LocationMapActivity : AppCompatActivity(), OnMapReadyCallback {
+class LocationMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityLocationMapBinding
@@ -39,10 +42,37 @@ class LocationMapActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.setOnMapLongClickListener(this);
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val location = LatLng(52.22, 21.01)
+        mMap.addMarker(MarkerOptions().position(location).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location))
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(
+                    location.latitude,
+                    location.longitude
+                ), 12.0f
+            )
+        )
+    }
+
+    /**
+     * After long press ask user if he wants to save selected location.
+     * */
+    override fun onMapLongClick(p0: LatLng) {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setMessage(resources.getString(R.string.alert_dialog_message))
+            .setCancelable(false)
+            .setPositiveButton(resources.getString(R.string.alert_dialog_yes)
+            ) { _, _ ->
+                Toast.makeText (applicationContext, "Saving", Toast.LENGTH_SHORT).show()
+            }.setNegativeButton(resources.getString(R.string.alert_dialog_no)){ _, _ ->
+            }
+
+        val alert = dialogBuilder.create()
+        alert.setTitle(resources.getString(R.string.alert_dialog_save))
+        alert.show()
     }
 }
